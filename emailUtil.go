49b0c94b1d7936ed0e8b6f0e1ddd64fa49640130
gloomysw <shaow@axon.com.cn>
2017-04-12 11:48:08 +0800

    修正变量命名

--- emailUtil.go
+++ emailUtil.go
@@ -10,10 +10,10 @@ import (
 
 // 发送邮件
 // create by gloomy 2017-04-12 11:18:23
-func SendEmail(account, passWord, smtpOption, emailSendPort, emailTitle string, sendContent *[]byte, toEmailUser []string) error {
-	auth := smtp.PlainAuth("", account, passWord, smtpOption)
+func SendEmail(account, passWord, smtpUrl, smtpUrlPort, emailTitle string, emailContent *[]byte, toEmailUser []string) error {
+	auth := smtp.PlainAuth("", account, passWord, smtpUrl)
 	to := toEmailUser
 	msg := []byte(fmt.Sprintf("To: %s \r\n Subject: %s \r\n Content-Type: text/plain; charset=UTF-8\r\n\r\n", strings.Join(toEmailUser, ";"), emailTitle))
-	msg = append(msg, (*sendContent)...)
-	return smtp.SendMail(fmt.Sprintf("%s:%s", smtpOption, emailSendPort), auth, account, to, msg)
+	msg = append(msg, (*emailContent)...)
+	return smtp.SendMail(fmt.Sprintf("%s:%s", smtpUrl, smtpUrlPort), auth, account, to, msg)
 }
