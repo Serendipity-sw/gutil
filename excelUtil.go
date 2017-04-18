9468840708946107c56ff2af4a81e6f4211d4b79
gloomysw <gloomysw>
2017-04-18 17:10:04 +0800

    参数值获取出错问题

--- excelUtil.go
+++ excelUtil.go
@@ -11,25 +11,27 @@ import (
 // excel数据获取
 // create by gloomy 2017-4-18 12:00:34
 // sheet名称 数据内容 错误对象
-func ReadExcel(excelFilePath string) (*map[string][]string, error) {
+func ReadExcel(excelFilePath string) (*map[string][][]string, error) {
 	xlFile, err := xlsx.OpenFile(excelFilePath)
 	if err != nil {
 		return nil, err
 	}
 	var (
-		excelData   map[string][]string = make(map[string][]string)
+		excelData   map[string][][]string = make(map[string][][]string)
 		columnValue string
 	)
 	for _, sheet := range xlFile.Sheets {
 		for _, row := range sheet.Rows {
+			var contentArray []string
 			for _, cell := range row.Cells {
 				columnValue, err = cell.String()
 				if err != nil {
 					fmt.Println("ReadExcel ", err.Error())
 					continue
 				}
-				excelData[sheet.Name] = append(excelData[sheet.Name], columnValue)
+				contentArray = append(contentArray, columnValue)
 			}
+			excelData[sheet.Name] = append(excelData[sheet.Name], contentArray)
 		}
 	}
 	return &excelData, nil
