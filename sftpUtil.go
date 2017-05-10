bd71b6754b6e5a2ed94904992ffd7b91d0e88532
gloomysw <shaow@axon.com.cn>
2017-05-10 12:07:38 +0800

    完成sftp连接方法 对象增加连接地址 增加sftp关闭方法 增加sftp读取文件夹内容方法

--- sftpUtil.go
+++ sftpUtil.go
@@ -3,8 +3,12 @@
 package gutil
 
 import (
+	"errors"
 	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+	"net"
+	"os"
 )
 
 // sftp配置类
@@ -14,9 +18,76 @@ type SftpConfigStruct struct {
 	Password     string // 登录密码
 	Port         int    // 服务器端口
 	ConntionSize int    // MaxPacket sets the maximum size of the payload
+	Addr         string // 连接地址
 }
 
 // 建立sftp连接
-func sftpConntion(sftpModel SftpConfigStruct, sftpClient *sftp.Client, sshClient *ssh.Client) {
+// create by gloomy 2017-05-10 11:32:17
+func sftpConntion(sftpModel SftpConfigStruct, sftpClient *sftp.Client, sshClient *ssh.Client) error {
+	var (
+		auths []ssh.AuthMethod
+		err   error
+	)
+	if aconn, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
+		auths = append(auths, ssh.PublicKeysCallback(agent.NewClient(aconn).Signers))
+	}
+	auths = append(auths, ssh.Password(sftpModel.Password))
+	configModel := ssh.ClientConfig{
+		User:            sftpModel.Account,
+		Auth:            auths,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	sshClient, err = ssh.Dial("tcp", sftpModel.Addr, &configModel)
+	if err != nil {
+		return err
+	}
+	sftpClient, err = sftp.NewClient(sshClient, sftp.MaxPacket(If(sftpModel.ConntionSize > 0, sftpModel.ConntionSize, 1<<15).(int)))
 
+	return err
+}
+
+// sftp 关闭
+// create by gloomy 2017-05-10 12:06:05
+func SftpClose(sftpClient *sftp.Client, sshClient *ssh.Client) error {
+	var (
+		errs     string
+		boNumber = 0
+	)
+	err := sftpClient.Close()
+	if err != nil {
+		errs += errors.New(err.Error())
+		boNumber++
+	}
+	err = sshClient.Close()
+	if err != nil {
+		errs += errors.New(err.Error())
+		return errors.New(errs)
+	}
+	if boNumber > 0 {
+		return errors.New(errs)
+	}
+	return err
+}
+
+// sftp读取文件夹内容
+// create by gloomy 2017-05-10 11:50:14
+func SftpReadDir(sftpModel SftpConfigStruct, sftpClient *sftp.Client, sshClient *ssh.Client, pathStr string) (*[]os.FileInfo, error) {
+	if sftpClient == nil || sshClient == nil {
+		err := sftpConntion(sftpModel, sftpClient, sshClient)
+		if err != nil {
+			return nil, err
+		}
+	}
+	files, err := sftpClient.ReadDir(pathStr)
+	if err != nil {
+		err := sftpConntion(sftpModel, sftpClient, sshClient)
+		if err != nil {
+			return nil, err
+		}
+		files, err = sftpClient.ReadDir(pathStr)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &files, err
 }
