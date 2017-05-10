32553602af8a6f0e00d9f6e9e2dea1bcfeb9abfc
gloomysw <gloomysw>
2017-05-10 18:10:23 +0800

    增加接口返回成功码

--- const.go
+++ const.go
@@ -1,5 +1,6 @@
 package gutil
 
 const (
-	WithNanos = "2006-01-02 15:04:05" // 时间格式化字符串
+	WithNanos           = "2006-01-02 15:04:05" // 时间格式化字符串
+	Result_Success_Code = "00000"               // 接口返回成功
 )
