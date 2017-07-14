1a35e95e5162769ffac94ecdb02d4fb34d88a945
gloomysw <gloomysw>
2017-07-14 12:21:23 +0800

    增加map转换XML方法

---
+++ stringMapStringMarshalXml.go
@@ -0,0 +1,35 @@
+// 增加map转换XML
+package gutil
+
+import "encoding/xml"
+
+// StringMap is a map[string]string.
+type LoadFile map[string]string
+
+// StringMap marshals into XML.
+func (s LoadFile) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+
+	tokens := []xml.Token{start}
+
+	for key, value := range s {
+		t := xml.StartElement{Name: xml.Name{"", key}}
+		tokens = append(tokens, t, xml.CharData(value), xml.EndElement{t.Name})
+	}
+
+	tokens = append(tokens, xml.EndElement{start.Name})
+
+	for _, t := range tokens {
+		err := e.EncodeToken(t)
+		if err != nil {
+			return err
+		}
+	}
+
+	// flush to ensure tokens are written
+	err := e.Flush()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
