71ca5d75b69f8824b1e1ae048ca90fea73c7325f
gloomysw <sw.gloomysw@gmail.com>
2018-12-24 15:32:02 +0800

    禁用方法

--- ginUtil.go
+++ ginUtil.go
@@ -2,17 +2,12 @@
 //create by gloomy 2017-09-01 01:11:28
 package gutil
 
-import (
-	"fmt"
-	"github.com/gin-gonic/gin"
-)
-
 //gin初始化
 //create by gloomy 2017-09-01 01:13:52
-func GinInit(debug bool, serverListeningPort int, rootPrefix string, setGinRouter func(r *gin.Engine, rootPrefix string)) *gin.Engine {
-	gin.SetMode(If(debug, gin.DebugMode, gin.ReleaseMode).(string))
-	rt := gin.Default()
-	setGinRouter(rt, rootPrefix)
-	go rt.Run(fmt.Sprintf(":%d", serverListeningPort))
-	return rt
-}
+//func GinInit(debug bool, serverListeningPort int, rootPrefix string, setGinRouter func(r *gin.Engine, rootPrefix string)) *gin.Engine {
+//	gin.SetMode(If(debug, gin.DebugMode, gin.ReleaseMode).(string))
+//	rt := gin.Default()
+//	setGinRouter(rt, rootPrefix)
+//	go rt.Run(fmt.Sprintf(":%d", serverListeningPort))
+//	return rt
+//}
